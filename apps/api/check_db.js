4b45bbc07c9ea2f206b43646907ebab5f8ba9355
Lucky <luckybhoir2230@gmail.com>
2026-09-05 01:59:21 +0530

    fix: add prisma generate to postinstall for CI

--- apps/api/check_db.js
+++
@@ -1,5 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const p = new PrismaClient();
-p.job.findFirst({ where: { id: 'cmtne4hpv000smp01ztpl76jr' }, include: { review: true } })
-  .then(j => console.log(JSON.stringify(j.review.staticAnalysis, null, 2)))
-  .finally(() => p.$disconnect());
